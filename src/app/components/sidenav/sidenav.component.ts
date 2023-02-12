5c7e0b7dfcf975a3b07de60d9744ef2cecb0f762
Petro Niemkov <pniemkov@gmail.com>
2023-02-12 15:22:16 -0500

    Drawer function

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { SidenavService } from '../../services/sidenav.service';
+import { MatDrawer } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.css']
 })
-export class SidenavComponent {
+export class SidenavComponent implements OnInit  {
+  //@ViewChild('drawer') public drawer: MatDrawer;
+
+  ngOnInit() { 
+
+   } 
 
 }
