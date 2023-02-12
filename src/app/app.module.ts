cadd018914bf5f36550cfe875d6e372ba17b61ce
Petro Niemkov <pniemkov@gmail.com>
2023-02-12 12:22:48 -0500

    side nav added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialExampleModule } from './material.module';
+import { Sidenav } from './components/sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialExampleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/sidenav/sidenav.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Autosize sidenav
+ */
+@Component({
+  selector: 'sidenav',
+  templateUrl: 'sidenav.component.html',
+  styleUrls: ['sidenav.component.css'],
+})
+export class Sidenav {
+  showFiller = false;
+}
